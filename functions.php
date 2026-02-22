e54052a83672081b61e7bd410af2607231700d37
Tomonori Mizuno <tomonori.m.mizuno@gmail.com>
2026-02-22 13:22:13 -0500

    doing

--- functions.php
+++ functions.php
@@ -1,6 +1,53 @@
 <?php
 
 /*************************************************
+ ルーティング
+**************************************************/
+
+function custom_rewrite_rule() {
+  /***** SDGS *****/
+  add_rewrite_rule('^sustainable/?$','index.php?page_sustainable=1','top');
+}
+add_action('init', 'custom_rewrite_rule', 10, 0);
+
+function custom_query_vars($vars){
+  $vars[] = 'page_sustainable';
+  return $vars;
+}
+add_filter('query_vars', 'custom_query_vars');
+
+/***** テンプレート *****/
+add_filter('template_include', function($template) {
+  // 固定ページ
+  if (get_query_var('page_sustainable')) {
+    $new_template = locate_template('page-sustainable.php');
+    if ($new_template) return $new_template;
+  }
+  return $template;
+});
+
+/*
+// リライトルールデバッグ
+add_action('template_redirect', function() {
+  global $wp, $wp_rewrite;
+  $rules = $wp_rewrite->wp_rewrite_rules();
+  if ( !empty($rules) ) {
+    foreach ($rules as $regex => $query) {
+      if (preg_match('#^' . $regex . '#', $wp->request, $matches)) {
+        echo '<pre>';
+        echo "Matched rule: " . esc_html($regex) . "\n";
+        echo "Query: " . esc_html($query) . "\n";
+        echo "Request: " . esc_html($wp->request) . "\n";
+        echo "Matches:\n";
+        print_r($matches);
+        echo '</pre>';
+        exit;
+      }
+    }
+  }
+});
+
+/*************************************************
  必要なオリジナル関数
 **************************************************/
 
@@ -34,11 +81,6 @@ remove_action('do_feed_rss2', 'do_feed_rss2');
 remove_action('do_feed_atom', 'do_feed_atom');
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
-
-require get_template_directory() . '/inc/custom-header.php';
-require get_template_directory() . '/inc/template-tags.php';
-require get_template_directory() . '/inc/extras.php';
-require get_template_directory() . '/inc/customizer.php';
 
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
---
+++ page-sustainable.php
@@ -0,0 +1,46 @@
+<?php
+
+$r = get_template_directory_uri();
+get_header(); ?>
+
+<section id="firstview">
+  <div class="container">
+    <h1 class="shippori">S&amp;Tを知る</h1>
+  </div>
+</section>
+
+<section id="introduction">
+  <div class="container">
+
+    <h2 class="shippori">不動産からの収益と資産価値の<br>最大化を図るために</h2>
+
+    <p>
+      不動産の活用や流通を取り巻く環境は、大きく変わろうとしてします。<br>
+      賃貸用不動産はその供給が実際の需要を大幅に上まわり、今後も高齢化、人口減少といった要因が<br>
+      ますます、その傾向に拍車をかけていきます。
+    </p>
+    <p>
+      オーナー様がご所有されているアパート・マンションやビルの「収益と資産価値の最大化」を<br>
+      事業目的をしている弊社にとって、これからますます厳しくなる賃貸経営のパートナーとして<br>
+      オーナー様にとって必要不可欠な存在になることを目指していくことが<br>
+      私たちの使命です。
+    </p>
+
+  </div>
+</section>
+
+<section id="produce">
+  <div class="container">
+
+    <div class="circle-area">
+      <img src="<?=$r?>/images/top/circle_st.png" alt="S&T">
+      <img src="<?=$r?>/images/top/icon_cross.png" alt="cross">
+      <img src="<?=$r?>/images/top/circle_more.png" alt="S&T more">
+    </div>
+
+    <h2 class="shippori" style="font-size:30px;">土地活用から賃貸経営へのトータルプロデュース</h2>
+
+  </div>
+</section> <?php
+
+get_footer();
